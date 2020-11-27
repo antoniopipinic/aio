575353b28c3e6bc85cb2e49942ceb852964dae79
Startklar <putz782@gmail.com>
2020-11-27 16:57:46 +0100

    AIO-8: books Table and listview added

--- src/main/java/org/openjfx/MainController.java
+++ src/main/java/org/openjfx/MainController.java
@@ -1,13 +1,46 @@
 package org.openjfx;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class MainController{
+    ObservableList<String> names = FXCollections.observableArrayList();
+    @FXML
+    private ListView<String> bookListView = new ListView<String>();
     @FXML
     protected void initialize(){
+        DBConnection connectNow = new DBConnection();
+        Connection connectDB = connectNow.getConnection();
+
+        String sqlString = "SELECT * FROM books";
+
+        try {
+            Statement statement = connectDB.createStatement();
+            ResultSet queryResult = statement.executeQuery(sqlString);
+
+            while(queryResult.next()){
+                names.add(queryResult.getString(2));
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+
+        bookListView.setItems(names);
     System.out.println("test");
     }
 }
